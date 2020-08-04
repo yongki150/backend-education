0844cf9927e3f523256f7c5af53036695f07f142
Brave Kim <ygflove@likelion.org>
2020-08-04 22:12:02 +0900

    change variable and into def

--- tasks/task03/yongki/main.py
+++ tasks/task03/yongki/main.py
@@ -2,13 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 from typing import List
 
-requests = requests.get("https://www.bible.ac.kr/ko/life/notice/list/1")
-html = requests.text
-soup = BeautifulSoup(html, 'html.parser')
-
 def get_notice() -> List[List[str]]:
+    req = requests.get("https://www.bible.ac.kr/ko/life/notice/list/1")
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+
     li_s = soup.select('.tbody')
-    lists = []
+    notice = []
 
     for li in li_s:
         num = li.select_one('.loopnum')
@@ -16,11 +16,11 @@ def get_notice() -> List[List[str]]:
         name = li.select_one('.name')
         reg_date = li.select_one('.reg_date')
 
-        list = [num.text, title.text, name.text, reg_date.text]
-        list[1] = list[1].replace("\n", "")
+        li_list = [num.text, title.text, name.text, reg_date.text]
+        li_list[1] = li_list[1].replace("\n", "")
 
-        lists.append(list)
-    return lists
+        notice.append(li_list)
+    return notice
 
 if __name__ == '__main__':
     for i in get_notice():
