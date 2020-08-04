c792c93a09dc5e095d3f662eab6af1ffe2b9906d
unknown <chojam301@gmail.com>
2020-08-04 23:24:05 +0900

    reupload task3

--- tasks/task03/jm/main.py
+++ tasks/task03/jm/main.py
@@ -2,23 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 from typing import List
 
-req = requests.get('https://www.bible.ac.kr/ko/life/notice').text
-soup = BeautifulSoup(req, 'html.parser')
-
 def get_notice() -> List[List[str]]:
-    hotkeys = soup.select('ul li.tbody')
+    req = requests.get('https://www.bible.ac.kr/ko/life/notice').text
+    soup = BeautifulSoup(req, 'html.parser')
+    lists = soup.select('ul li.tbody')
     temp_list = []
+    item = 0
 
-    for list in hotkeys:
+    for list in lists:
         num = list.select_one('.loopnum').get_text()
-        title = list.select_one('title').get_text()
+        title = list.select_one('.title').get_text()
         name = list.select_one('.name').get_text()
         date = list.select_one('.reg_date').get_text()
 
         temp_list.append([num, title, name, date])
 
-    return temp_list
+    for item in temp_list:
+        print(item)
+
+    return item
 
 if __name__ == '__main__':
-    for item in get_notice():
-        print(item)
+    get_notice()
