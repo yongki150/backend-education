e8996ea9a215fecee2c9352ee7c8821467be6580
unknown <chojam301@gmail.com>
2020-08-05 19:37:38 +0900

    Add target_page

--- tasks/task04/jm/main.py
+++ tasks/task04/jm/main.py
@@ -3,25 +3,28 @@ from typing import List
 import requests
 from bs4 import BeautifulSoup
 
-def get_notice_articles(page_num: int = 1) -> List[List[str]]:
-    page = f"https://www.bible.ac.kr/ko/life/notice/list/{page_num}"
+def target_page(page):
     request = requests.get(page).text
     soup = BeautifulSoup(request, 'html.parser')
-    table_list = soup.select('ul li.tbody')
+
+    return soup
+
+def get_notice_articles(page_num: int = 1) -> List[List[str]]:
+    page = f"https://www.bible.ac.kr/ko/life/notice/list/{page_num}"
+    table_list = target_page(page).select('ul li.tbody')
     outline = []
 
-
-    for urls in soup.select('div ul.black a'):
-        url = "https://www.bible.ac.kr" + urls.get('href')
-        req_link = requests.get(url).text
-        link = BeautifulSoup(req_link, 'html.parser')
-
     for element in table_list:
+        url = element.select_one('div ul.black a')['href']
+        url = "https://www.bible.ac.kr" + url
+
         title = element.select_one('.title').get_text()
         title = title.strip()
+
         name = element.select_one('.name').get_text()
         date = element.select_one('.reg_date').get_text()
-        article = link.select_one('.content').get_text()
+
+        article = target_page(url).select_one('.content').get_text()
         article = article.strip().replace("\xa0", "")
 
         outline.append([url, title, name, date, article])
