3ba5aa82e637146d7f7da7608203346d500ee395
Brave Kim <ygflove@likelion.org>
2020-08-05 15:15:05 +0900

    upload task04

---
+++ tasks/task04/yongki/main.py
@@ -0,0 +1,36 @@
+from typing import List
+
+import requests
+from bs4 import BeautifulSoup
+
+def parseUrl(url):
+    req = requests.get(url)
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    return soup
+
+def get_notice_articles(page_num: int = 1) -> List[List[str]]:
+    url = f"https://www.bible.ac.kr/ko/life/notice/list/{page_num}"
+    articles = []
+    li_s = parseUrl(url).select('.tbody')
+
+    for li in li_s:
+        link = li.select_one('.title > a')['href']
+        title = li.select_one('.title').text
+        name = li.select_one('.name').text
+        reg_date = li.select_one('.reg_date').text
+
+        link = "https://www.bible.ac.kr" + link
+        article = parseUrl(link).select_one('.content').text
+
+        title = title.replace("\n", "")
+        article = article.replace("\n", "").replace("\xa0", "")
+
+        li_list = [link, title, name, reg_date, article]
+        articles.append(li_list)
+    return articles
+
+if __name__ == '__main__':
+    for i in get_notice_articles(2):
+        print(i)
