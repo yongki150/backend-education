c7a3951830818fcfe896ed205347ba566e0d4428
unknown <chojam301@gmail.com>
2020-08-05 17:17:06 +0900

    add page_num

--- tasks/task04/jm/main.py
+++ tasks/task04/jm/main.py
@@ -4,10 +4,12 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_notice_articles(page_num: int = 1) -> List[List[str]]:
-    request = requests.get('https://www.bible.ac.kr/ko/life/notice').text
+    page = f"https://www.bible.ac.kr/ko/life/notice/list/{page_num}"
+    request = requests.get(page).text
     soup = BeautifulSoup(request, 'html.parser')
     table_list = soup.select('ul li.tbody')
     outline = []
+
 
     for urls in soup.select('div ul.black a'):
         url = "https://www.bible.ac.kr" + urls.get('href')
@@ -27,5 +29,5 @@ def get_notice_articles(page_num: int = 1) -> List[List[str]]:
     return outline
 
 if __name__ == '__main__':
-    for item in get_notice_articles(1):
+    for item in get_notice_articles(3):
         print(item)
