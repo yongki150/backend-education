c8f9a1e32b252d09d8667bd930a1199b6f0ac1c4
Brave Kim <ygflove@likelion.org>
2020-08-08 23:42:38 +0900

    fix: apply pep8

--- tasks/task05/yongki/main.py
+++ tasks/task05/yongki/main.py
@@ -1,10 +1,11 @@
 from typing import List
 from typing import Dict
-from typing import Set
+
 from collections import defaultdict
 
 import json
 import requests
+
 
 def get_timetable(items_res) -> Dict[str, List[List[str]]]:
     info = defaultdict(list)
@@ -20,17 +21,26 @@ def get_timetable(items_res) -> Dict[str, List[List[str]]]:
 
     return info
 
+
 def get_konkuk_movie_info(date: str) -> Dict[str, List[List[str]]]:
     url = "https://www.lottecinema.co.kr/LCWS/Ticketing/TicketingData.aspx"
-    param_list = {"MethodName": "GetPlaySequence", "channelType": "HO", "osType": "W",
-                "osVersion": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
-                "playDate": date, "cinemaID": "1|1|1004", "representationMovieCode": ""}
+    param_list = {
+        "MethodName": "GetPlaySequence",
+        "channelType": "HO",
+        "osType": "W",
+        "osVersion": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+                     "AppleWebKit/537.36 (KHTML, like Gecko)"
+                     "Chrome/84.0.4147.105 Safari/537.36",
+        "playDate": date,
+        "cinemaID": "1|1|1004",
+        "representationMovieCode": ""}
 
     parameter = {"paramList": json.dumps(param_list)}
-    res = requests.post(url, data= parameter).json()
+    res = requests.post(url, data=parameter).json()
     items_res = res['PlaySeqs']['Items']
 
     return get_timetable(items_res)
+
 
 if __name__ == '__main__':
     print(get_konkuk_movie_info("2020-08-08"))
