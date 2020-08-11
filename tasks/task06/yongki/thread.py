3d8e1f65a52d6a1b1f377748b94bd83b42ccccb6
Brave Kim <ygflove@likelion.org>
2020-08-11 15:53:45 +0900

    add: upload task06 files

---
+++ tasks/task06/yongki/thread.py
@@ -0,0 +1,47 @@
+from typing import List
+import unicodedata
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+import requests
+from bs4 import BeautifulSoup
+
+def get_article_from_url(url):
+    req = requests.get(url)
+    html = req.text
+
+    soup = BeautifulSoup(html, 'html.parser')
+    title = soup.select_one('.header > h5').text
+    content = soup.select_one('.content').text
+    name = soup.select_one('span[data-type="nickname"]').text
+    date = soup.select_one('time').text
+
+    title = title.replace("\n", "")
+    content = content.replace("\n", "")
+    content = unicodedata.normalize("NFKD", content)
+
+    return [title, name, content, date]
+
+
+def get_notice_articles_thread(page_num: int = 1) -> List[List[str]]:
+    url = f"https://www.bible.ac.kr/ko/life/notice/list/{page_num}"
+
+    req = requests.get(url)
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+    li_s = soup.select('.tbody')
+
+    async_base_url = "https://www.bible.ac.kr"
+
+    with ThreadPoolExecutor(max_workers=len(li_s)) as executor:
+        tasks = [executor.submit(get_article_from_url,
+                                 async_base_url + li.select_one('.title > a')['href']) for li in li_s]
+
+    return tasks
+
+if __name__ == '__main__':
+    st = time.time()
+    articles = get_notice_articles_thread(1)
+    for i in articles:
+        print(i.result())
+    print(time.time() - st)
