d14f0a6cceb135259294b945597cb87d987470e2
Brave Kim <ygflove@likelion.org>
2020-08-19 20:51:22 +0900

    feat: add condition when querystring is none

--- tasks/task08/yongki/main.py
+++ tasks/task08/yongki/main.py
@@ -15,7 +15,11 @@ async def handle(request):
     data = request.query
     query_sum = sum(int(val) for key, val in data.items())
 
-    response_obj = {'result': query_sum}
+    if query_sum == 0:
+        print("입력이 필요합니다.")
+        response_obj = {"stats": "failed", "message": "need input"}
+    else:
+        response_obj = {"status": "success", "result": query_sum}
     return web.Response(text=json.dumps(response_obj), status=200)
 
 
