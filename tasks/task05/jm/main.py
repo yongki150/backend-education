9cfa2bce11983d5c4f0dd2e1988d13225407f9e4
unknown <chojam301@gmail.com>
2020-08-20 05:41:15 +0900

    fix: remove overlapped element

--- tasks/task05/jm/main.py
+++ tasks/task05/jm/main.py
@@ -1,10 +1,11 @@
 from typing import Dict, List
 import json
+from collections import defaultdict
 
 import requests
 
 def get_konkuk_movie_info(date: str) -> Dict[str, List[List[str]]]:
-    item_list = {}
+    item_dict = defaultdict(list)
     url = "https://www.lottecinema.co.kr/LCWS/Ticketing/TicketingData.aspx"
     form_data = {
         "MethodName":"GetPlaySequence",
@@ -23,18 +24,17 @@ def get_konkuk_movie_info(date: str) -> Dict[str, List[List[str]]]:
     response = requests.post(url, data=parameters).json()
     movies_response = response['PlaySeqs']['Items']
 
-    table = []
     for item in movies_response:
         title = item["MovieNameKR"]
         start = item["StartTime"]
         end = item["EndTime"]
 
-        table.append([start, end])
-        table.sort()
+        item_dict[title].append([start, end])
 
-        item_list[title] = table
+    for i in item_dict:
+        item_dict[i].sort()
 
-    return item_list
+    return item_dict
 
 if __name__ == '__main__':
-    print(get_konkuk_movie_info("2020-08-19"))
+    print(get_konkuk_movie_info("2020-08-20"))
