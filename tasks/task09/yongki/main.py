d23225f061c7c362939c53effbde75f8853f8a17
Brave Kim <ygflove@likelion.org>
2020-08-25 17:22:53 +0900

    docs: remove intranet api

--- tasks/task09/yongki/main.py
+++ tasks/task09/yongki/main.py
@@ -1,52 +1,38 @@
 import asyncio
 
 from biblebot import LmsAPI
-from biblebot import IntranetAPI
+
+INFO = {
+    "ID": "",
+    "passWD": "",
+    "semester": "20202",
+}
 
 
-async def use_login_class():
+async def use_login_api():
     # Login
-    resp = await LmsAPI.Login.fetch("", "")
+    resp = await LmsAPI.Login.fetch(INFO["ID"], INFO["passWD"])
     result = LmsAPI.Login.parse(resp)
     cookie = result.data["cookies"]
-    # print(result)
+    print(result)
 
     # Get Profile
     resp = await LmsAPI.Profile.fetch(cookies=cookie)
     profile = LmsAPI.Profile.parse(resp)
-    # print(profile)
+    print(profile)
 
     # Get CourseList
-    resp = await LmsAPI.CourseList.fetch(cookies=cookie, semester="20202")
+    resp = await LmsAPI.CourseList.fetch(cookies=cookie, semester=INFO["semester"])
     result = LmsAPI.CourseList.parse(resp)
-    # print(result)
+    print(result)
 
     # Get Attendance
     resp = await LmsAPI.Attendance.fetch(cookies=cookie, course_code="1697")
     result = LmsAPI.Attendance.parse(resp)
-    # print(result)
-
-
-async def use_intranet_class():
-    # Login
-    response = await IntranetAPI.Login.fetch("", "")
-    result = IntranetAPI.Login.parse(response)
-    cookie = result.data["cookies"]
-    print(result)
-
-    # Get StudentPhoto
-    resp = await IntranetAPI.StudentPhoto.fetch(cookies=cookie, sid="201504009")
-    result = IntranetAPI.StudentPhoto.parse(resp)
-    # print(result)
-
-    # Get Chapel
-    resp = await IntranetAPI.Chapel.fetch(cookies=cookie, semester="20202")
-    result = IntranetAPI.Chapel.parse(resp)
     print(result)
 
 
 async def main():
-    await use_login_class()
-    await use_intranet_class()
+    await use_login_api()
 
 asyncio.run(main())
