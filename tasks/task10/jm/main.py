4b2f41338b956ac346f5f54314046035745f1a63
unknown <chojam301@gmail.com>
2020-08-28 15:52:22 +0900

    fix: login exception

--- tasks/task10/jm/main.py
+++ tasks/task10/jm/main.py
@@ -5,38 +5,36 @@ from biblebot import IntranetAPI, KbuAPI
 
 
 async def get_intranet_info():
-    try:
-        # IntranetAPI.Login
-        resp = await IntranetAPI.Login.fetch("", "")
-        if resp.status == 200:
-            raise ValueError
-        result = IntranetAPI.Login.parse(resp)
-        cookie = result.data["cookies"]
-        print(resp.status)
-        print(result)
+    # IntranetAPI.Login
+    resp = await IntranetAPI.Login.fetch("", "")
+    if resp.status == 200:
+        print("ID와 PW를 확인해주세요")
+        return ValueError
+    result = IntranetAPI.Login.parse(resp)
+    cookie = result.data["cookies"]
+    print(resp.status)
+    print(result)
 
-        # IntranetAPI.StudentPhoto
-        resp = await IntranetAPI.StudentPhoto.fetch(cookies=cookie, sid="")
-        'sid에 학번입력'
-        result = IntranetAPI.StudentPhoto.parse(resp)
-        print(result)
+    # IntranetAPI.StudentPhoto
+    resp = await IntranetAPI.StudentPhoto.fetch(cookies=cookie, sid="")
+    'sid에 학번입력'
+    result = IntranetAPI.StudentPhoto.parse(resp)
+    print(result)
 
-        # IntranetAPI.Chapel
-        resp = await IntranetAPI.Chapel.fetch(cookies=cookie)
-        result = IntranetAPI.Chapel.parse(resp)
-        print(result)
+    # IntranetAPI.Chapel
+    resp = await IntranetAPI.Chapel.fetch(cookies=cookie)
+    result = IntranetAPI.Chapel.parse(resp)
+    print(result)
 
-        # IntranetAPI.Timetable
-        resp = await IntranetAPI.Timetable.fetch(cookies=cookie)
-        result = IntranetAPI.Timetable.parse(resp)
-        pprint(result.data)
+    # IntranetAPI.Timetable
+    resp = await IntranetAPI.Timetable.fetch(cookies=cookie)
+    result = IntranetAPI.Timetable.parse(resp)
+    pprint(result.data)
 
-        # IntranetAPI.Course
-        resp = await IntranetAPI.Course.fetch(cookies=cookie)
-        result = IntranetAPI.Course.parse(resp)
-        pprint(result.data)
-    except ValueError:
-        print("ID와 PW를 확인해주세요.")
+    # IntranetAPI.Course
+    resp = await IntranetAPI.Course.fetch(cookies=cookie)
+    result = IntranetAPI.Course.parse(resp)
+    pprint(result.data)
 
 
 async def get_kbu_info():
