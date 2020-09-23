50a63b67895300b610f8b7c9e511f8648990948b
= <=>
2020-09-23 10:26:50 +0900

    task

--- tasks/task01/_task01_01.py
+++ tasks/task01/_task01_01.py
@@ -8,15 +8,17 @@ def _isN(s):
 
 ans = random.randrange(0, 1000)
 print(ans)
-#test print(type(ans) == type(pp))
 while(1):
     player = input()
     player = int(_isN(player))
-    #print("player_type", type(player), "player_val", player ,"ans_type", type(ans))
-    if(player == -1):
+    if player == -1:
         print("정수를 입력하세요")
-    elif( player == ans ):
+    elif player == ans :
         break
+    if player < ans:
+        print("UP")
+    else:
+        print("DOWN")
 print("정답입니다.")
 
 
--- tasks/task01/_task01_02.py
+++ tasks/task01/_task01_02.py
@@ -1,7 +1,5 @@
-#리스트에 들어있는 값을 활용해서 target값을 찾아내야함
-#문제에서, list의 len이 10^5이므로, N^2 알고리즘으로 만들면, TO남.
+from typing import List
 
-#음,.. 값을 정렬해서 양 뒤 쪽으로 부터 비교하여, 가장 최선의 해를 찾으면 될 듯.
 
 def _Fuc(_list, tg):
     i,j = 0, len(_list) - 1
@@ -16,13 +14,26 @@ def _Fuc(_list, tg):
         else:
             i += 1
     return False #불가능함 
-        
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        i, j = 0, len(nums) - 1
+        while(i < j):
+            _sum = nums[i] + nums[j]
+            print(i, " ", j, "  ", _sum)
+            if(_sum == target):
+                ans = [i, j]
+                print(ans)
+                return ans
+            elif(_sum > target): #더한 값이 더 클 경우
+                j -= 1
+            else:
+                i += 1
+        return False #불가능함 
 
 
 
-#_list = []
-_list = [3,10,20,40,4,7,9]
-_list.sort()
-print(_list)
-_tar = int(input())
-print(_Fuc(_list, _tar))
+if __name__ == "__main__":
+    assert Solution().twoSum([2, 7, 11, 15], 9) == [0, 1]
