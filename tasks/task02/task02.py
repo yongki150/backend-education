8ec71a442b80844ca65fa77c133758588e6ff4f6
PakaDopa <detols33@naver.com>
2020-10-08 12:35:05 +0900

    task02

--- tasks/task02/task02.py
+++ tasks/task02/task02.py
@@ -1,5 +1,5 @@
 import time
-import os.path
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -12,17 +12,15 @@ def _init_():
 
 
 def login(user_data):
-    this_path = os.path.dirname( os.path.abspath( __file__ ) )
-    path = os.path.join(this_path, 'dirvers')
-    dirver = webdriver.Chrome(path + '\\chromedriver.exe')
+    dirver = webdriver.Chrome('./drivers/chromedriver.exe')
     dirver.get('https://leetcode.com/accounts/login/')
     print(user_data)
     dirver.find_element_by_class_name('link__Mcl7').send_keys(Keys.ENTER)
-    dirver.find_element_by_xpath('//*[@id="identifierId"]').send_keys(user_data[0])
-    dirver.find_element_by_xpath('//*[@id="identifierNext"]/div/button').send_keys(Keys.ENTER)
+    dirver.find_element_by_xpath('//input[@type="email"]').send_keys(user_data[0])
+    dirver.find_element_by_xpath('//button[@jsname="LgbsSe"]').send_keys(Keys.ENTER)
     dirver.implicitly_wait(5)
-    dirver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send_keys(user_data[1])
-    dirver.find_element_by_xpath('//*[@id="passwordNext"]/div/button').send_keys(Keys.ENTER)
+    dirver.find_element_by_xpath('//input[@name="password"]').send_keys(user_data[1])
+    dirver.find_element_by_xpath('//button[@jsname="LgbsSe"]').send_keys(Keys.ENTER)
     time.sleep(500)
 
 
