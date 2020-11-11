99328a6ba57e2a97c697b433b70672ad220b5462
PakaDopa <detols33@naver.com>
2020-11-11 18:39:46 +0900

    upload(Revise04): task08.py

--- tasks/task08/task08.py
+++ tasks/task08/task08.py
@@ -1,19 +1,17 @@
-import asyncio
 from aiohttp import web
 import json
 
 routes = web.RouteTableDef()
 
 
-def __init__(loop):
-    app = web.Application(loop=loop)
+def __init__():
+    app = web.Application()
     app.add_routes(routes)
     return app
 
 
 def main():
-    loop = asyncio.get_event_loop()
-    app = __init__(loop)
+    app = __init__()
     web.run_app(app)
 
 
@@ -22,28 +20,28 @@ async def sum_(request):  # handler
     print(request)
     try:
         result = 0
-        keydatas = request.query.values()
+        key_data_query = request.query.values()
         # (a,b,c...)가 주어지지 않았을 때,
-        if len(keydatas) == 0:
+        if len(key_data_query) == 0:
             raise ValueError
         for key in request.query.values():
-            # key값 중 숫자가 아닌 수가 있을 경우
-            if key.isdigit() != True:
+            # key 값 중 숫자가 아닌 수가 있을 경우
+            if key.isdigit() is not True:
                 raise TypeError
             result += int(key)
-        response_obj = {"result": result}
+        response_obj = {"status": "success", "result": result}
         return web.Response(text=json.dumps(response_obj), status=200)
     except TypeError:
         response_obj = {"status": "failed", "message": "Input only integer"}
-        return web.Response(text=json.dumps(response_obj), status=500)
+        return web.Response(text=json.dumps(response_obj), status=400)
     except ValueError:
         response_obj = {"status": "failed", "message": "Input sum data"}
-        return web.Response(text=json.dumps(response_obj), status=500)
+        return web.Response(text=json.dumps(response_obj), status=400)
 
 
 @routes.get("/{name}", name="sum")
-async def Wrong_path_error(request):
-    return web.Response(text="wrong path! {}".format(request.match_info["name"]))
+async def wrong_path_error(request):
+    return web.Response(text="wrong path! 404 error! {}".format(request.match_info["name"]))
 
 
 if __name__ == "__main__":
